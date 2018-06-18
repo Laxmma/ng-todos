9a990d2e9397d0e616239c2e4f4065c7ef6bff9a
Nagarjuna Immadi <nagarjuna.immadi@thomsonreuters.com>
2018-06-18 10:38:23 +0530

    Added add-edit-todo component

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoService } from './todo.service';
+import { AddEditTodoComponent } from './add-edit-todo/add-edit-todo.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [TodoListComponent],
+  declarations: [TodoListComponent, AddEditTodoComponent],
   providers: [TodoService]
 })
 export class TodoModule { }
