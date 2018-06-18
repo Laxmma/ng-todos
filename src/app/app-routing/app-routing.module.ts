59399e3298fd739959ae4566fd78ae76a479039d
Nagarjuna Immadi <nagarjuna.immadi@thomsonreuters.com>
2018-06-18 10:43:52 +0530

    added add-edit-todo route

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -6,9 +6,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { TodoListComponent } from '../todo/todo-list/todo-list.component';
+import { AddEditTodoComponent } from '../todo/add-edit-todo/add-edit-todo.component';
 
 const appRoutes: Routes = [
   { path: 'todo-list', component: TodoListComponent },
+  { path: 'todo/:id', component: AddEditTodoComponent },
   { path: '', redirectTo: 'todo-list', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
